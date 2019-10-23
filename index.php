db6791fc19e02529f915eabd1f2f4e0970f16fcb
raultorres2603 <55553058+raultorres2603@users.noreply.github.com>
2019-10-23 15:44:43 +0200

    CREACIÓN DEL RECONOCIMIENTO DE VOZ

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
+  </head>
+  <body>
+    <input id="texto" type="text" placeholder="Lo que digas aparecerá aquí">
+    <button id="botonEmpezar" onclick="startRec()"> Escuchar </button>
+    
+    <script src="./js/script.js"></script>
+  </body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,60 @@
+// VARIABLE DE RECONOCIMIENTO DE VOZ //
+let rec;
+
+// OBJETOS DEL DOCUMENTO //
+var texto = document.getElementById('texto');
+var boton = document.getElementById('botonEmpezar');
+
+// ESTADO DEL RECONOCIMIENTO DE VOZ //
+var estadoRV = 0;
+
+// CONTADOR PARA METER EN VARIABLES //
+var contador = 0;
+
+// VARIABLES PARA UTILIZACIÓN //
+var n1 = 0;
+var n2 = 0;
+var n3 = 0;
+
+    if (!("webkitSpeechRecognition" in window)) { // Si no se puede utilizar la API webkitSpeechRecognition que muestre alerta else if crea el objeto //
+    	alert("No puedes usar ésta API en éste navegador");
+    } else {
+    	rec = new webkitSpeechRecognition(); // Iniciación del objeto //
+    	rec.lang = "es-ES"; // Lenguaje del objeto //
+    	rec.continuous = true; // Continuidad (no se para) //
+    	rec.interim = true;
+    	rec.addEventListener("result",iniciar); // Añadir evento //
+    }
+function iniciar(event){
+	for (let i = event.resultIndex; i < event.results.length; i++){
+         var escuchado = event.results[i][0].transcript; // Variable que coje el valor del reconociemiento una vez pasados unos 2 segundos //
+         texto.value = escuchado; // Asignar al input la variable escuchado //
+
+         if (contador == 0) { // If dependiendo de la variable contador //
+           n1 = escuchado;
+           contador++;
+         } else if (contador == 1) {
+            n2 = escuchado;
+            contador++;
+         } else if (contador == 2) {
+            n3 = escuchado;
+            alert(n1 + " " + n2 + " " + n3);
+            contador = 0;
+            n1 = 0;
+            n2 = 0;
+            n3 = 0;
+         }
+	}
+}
+
+function startRec() { // Función que inicia o para el reconocimiento de voz //
+  if (estadoRV == 0) {
+    rec.start();
+    boton.innerHTML = "Escuchando...";
+    estadoRV = 1;
+  } else if (estadoRV == 1) {
+    rec.stop();
+    boton.innerHTML = "Escuchar";
+    estadoRV = 0;
+  }
+}
